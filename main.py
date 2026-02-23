c0ecf3830b648e4375d658fb76dc9b2f09cfbb3d
Albin Babu Varghese <albinbabuvarghese20@gmail.com>
2026-02-22 19:58:08 -0500

    Add server response
    Also parse msg once using a variable

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import threading
 
 HOST = "0.0.0.0"
 
+def server_respond(conn, msg):
+    conn.sendall(msg.encode())
+    
 def end_connection(conn, addr):
     print("Closing connection with", addr)
+    server_respond(conn, "Closing Connection")
     conn.close()
     
 def handle_client(conn, addr):
        print("Connected by", addr)
        while True:
-           msg = conn.recv(1024)
-           if not msg or msg.decode().strip() == "/end":
+           msg = conn.recv(1024).decode().strip()
+           if not msg or msg == "/end":
                end_connection(conn, addr)
                break
-           print(addr, ":", msg.decode())
+           print(addr, ":", msg)
+           server_respond(conn, "Message Received")
 
 def socket_run():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
