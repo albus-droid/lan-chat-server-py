ecb4db9751fed9828e50e52572d3b7cfbd312776
Albin Babu Varghese <albinbabuvarghese20@gmail.com>
2026-02-22 19:23:16 -0500

    Add thread for multiple connections

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socket
+import thread
 
 HOST = "0.0.0.0"
 
@@ -7,24 +8,24 @@ def end_connection(conn, addr):
     print("Closing connection with", addr)
     conn.close()
     
+def handle_client(conn, addr):
+       print("Connected by", addr)
+       while True:
+           msg = conn.recv(1024)
+           if msg.decode().strip() == "/end":
+               end_connection(conn, addr)
+               break
+           print(addr, ":", msg.decode())
+
 def socket_run():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, 8080))
     s.listen()
-
     while True:
         conn, addr = s.accept()
-        print("Connected by", addr)
-
-        while True:
-            msg = conn.recv(1024)
-            if msg.decode().strip() == "/end":
-                end_connection(conn, addr)
-                break
-
-            print(addr, ":", msg.decode())
+        threading.Thread(target=handle_client, args=(conn, addr)).start()
+        
 
 if __name__ == "__main__":
-    socket_run()
+     socket_run()
