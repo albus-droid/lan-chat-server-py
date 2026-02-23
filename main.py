82a62214c53756d5c749b9fe48f292d3b89ab215
Albin Babu Varghese <albinbabuvarghese20@gmail.com>
2026-02-22 19:06:50 -0500

    Add command to end stream

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import socket
 
 HOST = "0.0.0.0"
 
+def end_connection(conn, addr):
+    print("Closing connection with", addr)
+    conn.close()
+    
 def socket_run():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, 8080))
@@ -12,11 +16,10 @@ def socket_run():
         print("Connected by", addr)
         while True:
             msg = conn.recv(1024)
-            if not msg:
+            if msg.decode().strip() == "/end":
+                end_connection(conn, addr)
                 break
             print(addr, ":", msg.decode())
-        conn.close()
-        print("Disconnected by", addr)
 
 if __name__ == "__main__":
     socket_run()
