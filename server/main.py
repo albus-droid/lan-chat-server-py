83b7f0a5bb8e3ee6e593ac1f1432f0d3203dd962
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2026-02-25 02:03:28 +0000

    add config for logging

--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 # main.py
 import signal
+import logging
+
 from server import Server
 
 def main():
     server = Server(host="0.0.0.0", port=8080)
-
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
     # shutdown signal handlers
     signal.signal(signal.SIGINT, lambda s, f: server.shutdown())
     signal.signal(signal.SIGTERM, lambda s, f: server.shutdown())
